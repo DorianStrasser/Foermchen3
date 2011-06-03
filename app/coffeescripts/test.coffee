32e87d0e0b3609c086da3383722b3835e4f15be5
Dorian Strasser <dorian.strasser@gmail.com>
2011-06-03 16:58:08 +0300

    Sliders work

--- app/coffeescripts/test.coffee
+++
@@ -1,4 +0,0 @@
-$ ->
-    $( "#slider_interest2" ).slider { value:100, min: 0, max: 500, step: 50, slide: ( event, ui ) ->
-        $( "#interest_calculator_interest2" ).val( "$" + ui.value )	}
-    $( "#interest_calculator_interest2" ).val( "$" + $( "#slider_interest2" ).slider( "value" ) )
--- public/javascripts/calc.js
+++ public/javascripts/calc.js
@@ -1,12 +1,32 @@
+var Step = 0.25
 $(function() {
 		$( "#slider_interest1" ).slider({
-			value:100,
+			value:$( "#interest_calculator_interest1" ).val(),
 			min: 0,
-			max: 500,
-			step: 50,
+			max: 20,
+            step: Step,
 			slide: function( event, ui ) {
-				$( "#interest_calculator_interest1" ).val( "$" + ui.value );
+				$( "#interest_calculator_interest1" ).val( ui.value );
 			}
 		});
-		$( "#interest_calculator_interest1" ).val( "$" + $( "#slider_interest1" ).slider( "value" ) );
+		$( "#interest_calculator_interest1" ).val( $( "#slider_interest1" ).slider( "value" ) );
+
+        $( "#interest_calculator_interest1" ).keyup(function (){
+            $("#slider_interest1").slider("value", $("#interest_calculator_interest1").val());
+        });
+
+        $( "#slider_interest2" ).slider({
+			value:$( "#interest_calculator_interest2" ).val(),
+			min: 0,
+			max: 20,
+            step: Step,
+			slide: function( event, ui ) {
+				$( "#interest_calculator_interest2" ).val( ui.value );
+			}
+		});
+		$( "#interest_calculator_interest2" ).val( $( "#slider_interest2" ).slider( "value" ) );
+
+        $( "#interest_calculator_interest2" ).keyup(function (){
+            $("#slider_interest2").slider("value", $("#interest_calculator_interest2").val());
+        });
 	});
